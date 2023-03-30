99014ddc30e5cd5d2911f30beac80dd445e17b06
nathanluc <nathanluc08@gmail.com>
2023-03-30 12:03:49 +0200

    Table Server

--- src/app/Server/table-server/table-server.component.ts
+++ src/app/Server/table-server/table-server.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
 import { resSubject } from "../../services/api-server.service";
 import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
 @Component({
   selector: 'app-table-server',
   templateUrl: './table-server.component.html',
@@ -9,12 +10,14 @@ import { MatPaginator } from "@angular/material/paginator";
 })
 export class TableServerComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator | any;
+  @ViewChild(MatSort) sort: MatSort | any;
   displayedColumns: string[] = ['uuid', 'hostname', 'ip', 'os', 'version', 'lastseen', 'status', 'create'];
   dataSource: any;
   ngOnInit(): void {
     resSubject.subscribe(res => {
       this.dataSource = new MatTableDataSource(res.body);
       this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
   }
 }
